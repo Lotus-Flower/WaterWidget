fbafac0d3dcc5a8f8e4f635694f8cac40a861ba6
Matthew <mtmeehan6853@gmail.com>
2021-12-18 14:06:06 -0700

    First commit

---
+++ app/src/main/java/meehan/matthew/waterwidget/WaterWidgetActions.kt
@@ -0,0 +1,38 @@
+package meehan.matthew.waterwidget
+
+import android.content.Context
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.glance.GlanceId
+import androidx.glance.action.ActionParameters
+import androidx.glance.appwidget.action.ActionCallback
+import androidx.glance.appwidget.state.updateAppWidgetState
+import androidx.glance.state.PreferencesGlanceStateDefinition
+import meehan.matthew.waterwidget.WaterWidget.Companion.MAX_GLASSES
+import meehan.matthew.waterwidget.WaterWidget.Companion.WATER_WIDGET_PREFS_KEY
+
+class AddWaterClickAction : ActionCallback {
+    override suspend fun onRun(context: Context, glanceId: GlanceId, parameters: ActionParameters) {
+        updateAppWidgetState(context, PreferencesGlanceStateDefinition, glanceId) {
+            it.toMutablePreferences()
+                .apply {
+                    val glassesOfWater = this[intPreferencesKey(WATER_WIDGET_PREFS_KEY)] ?: 0
+                    if (glassesOfWater < MAX_GLASSES) {
+                        this[intPreferencesKey(WATER_WIDGET_PREFS_KEY)] = glassesOfWater + 1
+                    }
+                }
+        }
+        WaterWidget().update(context, glanceId)
+    }
+}
+
+class ClearWaterClickAction : ActionCallback {
+    override suspend fun onRun(context: Context, glanceId: GlanceId, parameters: ActionParameters) {
+        updateAppWidgetState(context, PreferencesGlanceStateDefinition, glanceId) {
+            it.toMutablePreferences()
+                .apply {
+                    this[intPreferencesKey(WATER_WIDGET_PREFS_KEY)] = 0
+                }
+        }
+        WaterWidget().update(context, glanceId)
+    }
+}
